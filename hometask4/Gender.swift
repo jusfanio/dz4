0e8b094a86560fa51a3d54621b7a71c3d122fcc4
Jusfan <jusfanio@Alesias-MacBook-Pro.local>
2017-09-24 14:43:20 +0300

    dz4-all

---
+++ hometask4/Gender.swift
@@ -0,0 +1,13 @@
+//
+//  Gender.swift
+//  hometask4
+//
+//  Created by Jusfan on 23/09/2017.
+//  Copyright © 2017 Alesia. All rights reserved.
+//
+
+import Foundation
+
+enum Gender {
+    case male, female
+}
--- hometask4/ViewController.swift
+++ hometask4/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let firstDragon = Dragon(eyesColor: .green, tailLength: 5, hasFire: true, weight: 170, gender: Gender.male, growth: 7.0, name: "Balerion")
+    
+    @IBOutlet weak var hungryLabel: UILabel!
+    @IBOutlet weak var actionLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var tailLengthLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        firstDragon.fly(changeSpeed: 20.0)
+        print(firstDragon.speed)
+        
+        firstDragon.eat(food: 10)
+        
+        changeUI()
+    }
+    
+    func changeUI() {
+        hungryLabel.text = "\(firstDragon.isHungry())"
+        
+        actionLabel.text = "\(firstDragon.action)"
+        
+        weightLabel.text = "\(firstDragon.weight)"
+        
+        tailLengthLabel.text = "\(firstDragon.tailLength)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func eatAction(_ sender: UIButton) {
+        firstDragon.eat(food: 15)
+        changeUI()
     }
-
-
+    @IBAction func flyAction(_ sender: UIButton) {
+        firstDragon.fly(changeSpeed: 10)
+        changeUI()
+    }
 }
+
+
+
 
